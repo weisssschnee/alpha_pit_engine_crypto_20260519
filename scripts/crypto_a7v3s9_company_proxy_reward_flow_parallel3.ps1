8150d7c1e92f42b6e4175db3ad24853b65a5a0c9
weisssschnee <125790657+weisssschnee@users.noreply.github.com>
2026-06-14 15:10:35 +0800

    add crypto v3 proxy flow low parallel runner

---
+++ scripts/crypto_a7v3s9_company_proxy_reward_flow_parallel3.ps1
@@ -0,0 +1,3 @@
+$ErrorActionPreference = "Stop"
+$env:A7V3S9_MAX_PARALLEL = "3"
+& "D:\HermesWorker\GDrive\Project_V7_Rotation\alpha_pit_engine_crypto_20260519_remote\scripts\crypto_a7v3s9_company_proxy_reward_flow.ps1"
